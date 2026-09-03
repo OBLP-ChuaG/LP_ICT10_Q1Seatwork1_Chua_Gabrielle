1d3b1fb9970f15c65ca98e15b5df8af82bf769d7
mango <chua.gc@obmontessori.edu.ph>
2026-09-04 07:39:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+studname = "Manila Martinez" #String
+studage = "15" #Integer
+stud_height = "154" #Floating-point
+stud_country_to_visit = ["Japan", "Thailand", "Switzerland"] #List
+stud_new = False #boolean
+stud_info= {"color":"blue", 
+            "car_brand":"Tesla", 
+            "shoe_size":"7", 
+            "bff":"Gabby Chua" 
+        } #Dictionary
+stud_favefruits = {
+    "Grapes",
+    "Mangosteen",
+    "Banana",
+    "Rambutan",
+    "Durian"
+} #Set
+days_of_week = (
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+) #Tuple
+
+print("Name:" , studname)
+print("Age:" , studage)
+print("Height:" , stud_height)
+print("Country:" , stud_country_to_visit)
+print("Status:" , stud_new)
+print("Info:" , stud_info)
+print("Fruits:" , stud_favefruits)
